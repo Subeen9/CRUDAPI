495c70829a262c63a6df2c5f2542289d573023c5
Subeen9 <subin.bista@selu.edu>
2024-07-02 12:28:09 -0500

    Model created with get and post

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const app = express()
+app.use(express.json())
+const model = require('./models/product.model.js')
 const port = 3000;
 const mongoose = require('mongoose')
 
 app.get('/', (req, res)=>{
     res.send("You are on my server. local host 3000")
+})
+app.get("/api/get",async(req,res)=>{
+    try{
+        const user = await model.find({});
+        res.status(200).json(user)
+    }
+    catch(e){
+        res.status(500).json(e.message)
+    }
+})
+// Get by Id
+app.get("/api/get/:id", async (req, res)=>{
+    try{
+        const {id } =  req.params;
+        const user = await model.findById(id)
+        res.status(200).json(user)
+    }
+    catch(e){
+        res.status(500).json({message: e.message})
+    }
+})
+app.post('/api/user', async(req,res)=>{
+    try{
+        const user = await model.create(req.body)
+        res.status(200).json(user)
+    }
+    catch(error){
+        res.status(500).json({message:error.message})
+    }
 })
 mongoose.connect("mongodb+srv://subin:YloVABV7fMsg7nby@cluster0.0yidtdz.mongodb.net/Node?retryWrites=true&w=majority&appName=Cluster0")
 .then(()=>{
--- models/product.model.js
+++ models/product.model.js
@@ -1,1 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  bio: {
+    type: String,
+    maxlength: 500,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  UpdatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  imageUrl: {
+    type: String,
+    required: false,
+  },
+});
+const user = mongoose.model("user", userSchema)
+module.exports = user;
